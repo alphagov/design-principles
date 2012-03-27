84459d6de5f9b626a558cd8bfd57308074d06851
Frances Berriman <fberriman@gmail.com>
2012-03-27 16:28:10 +0100

    Toggle just examples, with class name

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -2,25 +2,14 @@ jQuery(document).ready(function() {
   
   $("body").addClass("js-enabled");
 
-  sectionToggles("why");
+  //sectionToggles("why");
   sectionToggles("examples");
 
   function sectionToggles(section){
     $("."+section+" h2").on("click", function(){
-        $(this).next().toggle("slow", function(){
-
-        });
+        $(this).next().toggleClass("open-section");
     });
-    $("."+section+" h2").hover(
-      function () {
-        $(this).addClass("hover");
-      },
-      function () {
-        $(this).removeClass("hover");
-      }
-    );
   }
-  
 });
 
 
