6c04ebb8b4246e97a9118a49ff86ade7c24b4afb
Frances Berriman <fberriman@gmail.com>
2012-03-27 15:52:08 +0100

    Hide/show sections, and add a hover class for effect on h2

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -4,6 +4,26 @@ jQuery(document).ready(function() {
 
   //$("ol li.principle").slidedeck({navigation: "nav a", keyevents: true, urls: false, fullheight: true});
 
+  $(".content").css("display", "none")
+  sectionToggles("why");
+  sectionToggles("examples");
+
+  function sectionToggles(section){
+    $("."+section+" h2").on("click", function(){
+        $(this).next().toggle("slow", function(){
+
+        });
+    });
+    $("."+section+" h2").hover(
+      function () {
+        $(this).addClass("hover");
+      },
+      function () {
+        $(this).removeClass("hover");
+      }
+    );
+  }
+  
 });
 
 
