7bff10aa9f725d5a6ae7e2246b5e7b14fa97b469
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2012-10-03 14:41:51 +0100

    IE& fix to style guide

---
+++ _site/app/assets/javascripts/main.js
@@ -0,0 +1,19 @@
+jQuery(document).ready(function() {
+
+  sectionToggles("examples");
+
+  function sectionToggles(section){
+  	$("."+section+" h2").each(function(){
+
+  		var text = $(this).html("<a href=''>"+$(this).text()+"</a>");
+
+  	})
+    $("."+section+" h2").on("click", function(e){
+        $(this).parent(".examples").toggleClass("open-section");
+        e.preventDefault();
+        return false;
+    });
+  }
+});
+
+
