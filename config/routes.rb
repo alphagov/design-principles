7c8c4e604a579366b20105f15696c52b69b66624
russell thorn <russell.thorn@digital.cabinet-office.gov.uk>
2013-01-30 16:24:14 +0000

    renaming files and adding to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 DesignPrinciples::Application.routes.draw do
 
-  get "/designprinciples", :to => "root#designprinciples"
+  get "/design-principles", :to => "root#design-principles"
   get "/designprinciples/styleguide", :to => "root#styleguide"
   get "/designprinciples/mainstream", :to => "root#mainstream"
-  get "/designprinciples/insidegovernment", :to => "root#insidegovernment"
+  get "/designprinciples/inside-government", :to => "root#inside-government"
+  get "/designprinciples/detailed-guides", :to => "root#detailed-guides"
+  get "/designprinciples/accessible-pdfs", :to => "root#accessible-pdfs"
   get "/designprinciples/performanceframework", :to => "root#performanceframework"
-  get "/designprinciples/whatsnew", :to => "root#whatsnew"
+  get "/designprinciples/whats-new", :to => "root#whats-new"
 
   root :to => redirect("/designprinciples")
 end
