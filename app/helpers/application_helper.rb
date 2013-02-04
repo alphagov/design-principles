64d7f713c262db1adbed811b8ffba6348afbd8dc
David Thompson <david.thompson@digital.cabinet-office.gov.uk>
2013-02-04 16:10:03 +0000

    Render the style guide navigation from a template.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  def nav_link_to(body, url_options = {}, html_options = {})
+    if current_page?(url_options)
+      old_class = html_options.fetch(:class, "")
+      new_class = old_class.split.push("current").join(" ")
+      link_to(body, url_options, html_options.merge(class: new_class))
+    else
+      link_to(body, url_options, html_options)
+    end
+  end
 end
