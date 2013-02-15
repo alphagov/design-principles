6f584e1eb1d50b032131f8b2a1f1b46c8803b167
Mark Hurrell <mark.hurrell@digital.cabinet-office.gov.uk>
2013-02-15 15:14:50 +0000

    Completely new css and refactored nav html

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,18 @@
 module ApplicationHelper
 
-  def nav_link_to(body, url_options = {}, html_options = {})
+  def nav_link_to(text, url_options = {}, html_options = {}, &block)
+    unless text.is_a? String
+      html_options = url_options
+      url_options = text
+      text = capture(&block)
+    end
+    
     if current_page?(url_options)
       old_class = html_options.fetch(:class, "")
       new_class = old_class.split.push("current").join(" ")
-      link_to(body, url_options, html_options.merge(class: new_class))
+      link_to(text, url_options, html_options.merge(class: new_class))
     else
-      link_to(body, url_options, html_options)
+      link_to(text, url_options, html_options)
     end
   end
 end
