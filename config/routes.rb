636cb61daac823838a92874f2654727ed234fa09
James Stewart <jys@ketlai.co.uk>
2013-03-20 11:10:02 +0000

    Move /service-manual/search to /service-manual/testsearch to help with testing

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,6 @@ DesignPrinciples::Application.routes.draw do
   get "/designprinciples/performanceframework", :to => "root#performanceframework"
   get "/designprinciples/whatsnew", :to => "root#whatsnew"
 
-  get "/:prefix/search", :to => 'search#index'
+  get "/:prefix/testsearch", :to => 'search#index'
   root :to => redirect("/designprinciples")
 end
