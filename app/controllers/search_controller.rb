4248b9cf80413d7ce25842c18a8bc7e9685f66b9
James Stewart <jys@ketlai.co.uk>
2013-04-14 09:41:42 +0100

    Very basic test coverage for searching
    Little more than a sanity check of the code. This involved a slight refactor
    of the controller to make the search client easier to mock, and the addition
    of the mocha gem.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,14 +8,18 @@ class SearchController < ApplicationController
 
     set_expiry
 
-    @client = GdsApi::Rummager.new(Plek.current.find('search') + '/specialist')
-    res = @client.search(params[:q])
+    res = search_client.search(params[:q])
     @results = res.map { |r| SearchResult.new(r) }
   rescue GdsApi::Rummager::SearchServiceError => e
     @results = ["HELP!"]
   end
 
   protected
+
+  def search_client
+    @search_client ||= GdsApi::Rummager.new(Plek.current.find('search') + '/specialist')
+  end
+
   def set_expiry(duration = 30.minutes)
     unless Rails.env.development?
       expires_in(duration, :public => true)
