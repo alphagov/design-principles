alphagov/design-principles

commit 19d4ec72279f73138a77e092d278d49dbda6803f

Use the newer hash response format for searches
This is part of upgrading all clients, and then later we can make the hash
response_style the default.

app/controllers/search_controller.rb
@@ -10,9 +10,9 @@ class SearchController < ApplicationController
 
     response.headers[Slimmer::Headers::SEARCH_PATH_HEADER] = '/service-manual/search'
 
-    res = search_client.search(params[:q])
+    res = search_client.search(params[:q], response_style: "hash")
     @search_term = params[:q]
-    @results = res.map { |r| SearchResult.new(r) }
+    @results = res["results"].map { |r| SearchResult.new(r) }
   rescue GdsApi::BaseError => e
     @results = ["HELP!"]
   end

spec/controller/search_controller_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 
 describe SearchController, :type => :controller do
   def stub_client
-    stub_search_client = stub("search", search: [])
+    stub_search_client = stub("search", search: { "results" => [] })
     controller.stubs(:search_client).returns(stub_search_client)
   end
 
@@ -16,12 +16,15 @@ describe SearchController, :type => :controller do
   end
 
   it "should pass our query parameter in to the search client" do
-    controller.search_client.expects(:search).with("search-term", nil).returns([]).once
+    controller.search_client.expects(:search)
+                            .with("search-term", response_style: "hash")
+                            .returns("results" => []).once
     do_search
   end
 
   it "should return unlimited results" do
-    controller.search_client.stubs(:search).returns(Array.new(75, {}))
+    controller.search_client.stubs(:search)
+                            .returns("results" => Array.new(75, {}))
 
     do_search('Test')
 

commit 6ed1163378abec1e257f3845728b56dc9d015e21

Handle the query string being empty
We were returning an error becuase we expected the Rummager response to be a
hash, but it was an array. This is because gds-api-adapters was "helpfully"
returning the equivalent of 0 results if the query was blank.

design-principles has been broken in this respect since:
19d4ec72279f73138a77e092d278d49dbda6803f

Although it doesn't explicitly tell the user why they've got no results, it
should make sense that blank query equals no results.

app/controllers/search_controller.rb
@@ -10,9 +10,14 @@ class SearchController < ApplicationController
 
     response.headers[Slimmer::Headers::SEARCH_PATH_HEADER] = '/service-manual/search'
 
-    res = search_client.search(params[:q], response_style: "hash")
     @search_term = params[:q]
-    @results = res["results"].map { |r| SearchResult.new(r) }
+
+    if params[:q].blank?
+      @results = []
+    else
+      res = search_client.search(params[:q], response_style: "hash")
+      @results = res["results"].map { |r| SearchResult.new(r) }
+    end
   rescue GdsApi::BaseError => e
     @results = ["HELP!"]
   end

spec/controller/search_controller_spec.rb
@@ -22,6 +22,11 @@ describe SearchController, :type => :controller do
     do_search
   end
 
+  it "should handle a blank query" do
+    controller.search_client.expects(:search).never
+    do_search('')
+  end
+
   it "should return unlimited results" do
     controller.search_client.stubs(:search)
                             .returns("results" => Array.new(75, {}))