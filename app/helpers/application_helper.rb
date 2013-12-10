7fbab37a7db3ac00e2ad804ba77426bf701b5db5
James Abley <james.abley@gmail.com>
2013-12-10 13:02:05 +0000

    Link to the beginning of the content
    This feels horrible. Ben asked for this.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,8 +6,11 @@ module ApplicationHelper
       url_options = text
       text = capture(&block)
     end
+
+    current_options = url_options.clone
+    current_options.delete(:anchor)
     
-    if current_page?(url_options)
+    if current_page?(current_options)
       old_class = html_options.fetch(:class, "")
       new_class = old_class.split.push("current").join(" ")
       link_to(text, url_options, html_options.merge(class: new_class))
