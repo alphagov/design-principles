cee044cdba65752fd56bd3a8558264807d821297
Vinay Patel <mail@vinayvinay.com>
2013-12-18 17:44:42 +0000

    Enabling Logstasher in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,4 +60,9 @@ DesignPrinciples::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  # Enable JSON-style logging
+  config.logstasher.enabled = true
+  config.logstasher.logger = Logger.new("#{Rails.root}/log/#{Rails.env}.json.log")
+  config.logstasher.supress_app_log = true
 end
