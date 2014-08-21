4c8628f59d7401618b868f2c354228b18bcf16c2
James Abley <james.abley@gmail.com>
2014-08-21 11:23:46 +0100

    Do the hard work to give more precise error

--- spec/markup_spec.rb
+++ spec/markup_spec.rb
@@ -6,9 +6,38 @@ require 'spec_helper'
 describe :erb do
   it "should not contain markdown artifacts" do
     files_with_markdown = Dir['app/**/*.erb'].map { |f|
-      [f, (File.read(f) =~ /\[[^\]]+\]\([^\)]+\)/)]
+      s = File.read(f)
+      pos = (s =~ /\[[^\]]+\]\([^\)]+\)/)
+      if !pos.nil?
+        # pos is string length. Try to convert that into a file offset
+        pos = s.slice(0, pos).bytesize
+      end
+      [f, pos ]
     }.select { |_, position|
       !position.nil?
+    }.map { |filename, position|
+      res = filename
+      line = 1
+      col = 0
+
+      File.open(filename) do |f|
+        (0..position).each do |i|
+          b = f.getbyte
+
+          if b == 0x0A # newline
+            line += 1
+            col = 1
+          else
+            # is this start byte of a UTF-8 codepoint?
+            col += 1 if ((b & 0xC0) != 0x80)
+          end
+        end
+
+        # location is vague since position isn't a file offset
+        res += " around line #{line}, column #{col}"
+      end
+
+      res
     }
     expect(files_with_markdown).to be_empty, files_with_markdown
   end
