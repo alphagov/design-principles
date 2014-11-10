394b9d47fdb039dac6702db0d1468b54ecc0d747
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2014-11-10 12:45:21 +0000

    Build assets as part of CI job.
    This will allow us to catch any errors early.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+set -e
+
 export RAILS_ENV=test
 
 git clean -fdx
@@ -7,3 +9,4 @@ git clean -fdx
 bundle install --path "${HOME}/bundles/${JOB_NAME}" --deployment
 
 bundle exec rake
+GOVUK_APP_DOMAIN=test.gov.uk bundle exec rake assets:precompile
