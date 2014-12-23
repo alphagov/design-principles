d8fdf9edb176b7fb3d27788bd74c1b3daba06c4c
James Abley <james.abley@gmail.com>
2014-12-23 11:25:18 +0000

    Remove all the old style guide content
    This is now published in guidance. The only thing still dangling is the
    Atom feed.
    
    We should also look to clean up the v1 style guide URLs so that they are
    in the router and don’t need to live in this application.

--- app/controllers/styleguide_controller.rb
+++ app/controllers/styleguide_controller.rb
@@ -4,95 +4,10 @@ class StyleguideController < ApplicationController
 
   def index
     respond_to do |format|
-      format.html { render "writing-for-govuk" }
       format.atom do
         render "whats-new"
       end
     end
-  end
-
-  def writing_for_govuk
-    render "writing-for-govuk"
-  end
-
-  def writing_for_the_web
-    render "writing-for-the-web"
-  end
-
-  def style_points
-    render "style-points"
-  end
-
-  def answers
-  end
-
-  def benefits_and_schemes
-    render "benefits-and-schemes"
-  end
-
-  def case_studies
-    render "case-studies"
-  end
-
-  def consultations
-  end
-
-  def corporate_information
-    render "corporate-information"
-  end
-
-  def detailed_guides
-    render "detailed-guides"
-  end
-
-  def document_collections
-  end
-
-  def government_responses
-    render "government-responses"
-  end
-
-  def groups
-  end
-
-  def guides
-  end
-
-  def images
-  end
-
-  def news_stories_and_press_releases
-    render "news-stories-and-press-releases"
-  end
-
-  def organisation_homepage
-    render "organisation-homepage"
-  end
-
-  def people_and_roles
-    render "people-and-roles"
-  end
-
-  def policy_pages
-    render "policy-pages"
-  end
-
-  def speeches
-  end
-
-  def statements_to_parliament
-    render "statements-to-parliament"
-  end
-
-  def publication_scheme
-  end
-
-  def whats_new
-    render "whats-new"
-  end
-
-  def a_z
-    render "a-z"
   end
 
 private
