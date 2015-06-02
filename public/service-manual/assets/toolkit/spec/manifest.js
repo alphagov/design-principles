5354f1964f42011680025efbbba65d05d7e0005e
Paul Hayes <fofr@users.noreply.github.com>
2015-06-02 12:16:17 +0100

    Service manual version 137
    * Remove classic analytics
    * Upgrade frontend_toolkit to 4.0.0
    
    https://github.com/alphagov/government-service-design-manual/compare/ver
    sion-136...version-137

--- public/service-manual/assets/toolkit/spec/manifest.js
+++ public/service-manual/assets/toolkit/spec/manifest.js
@@ -6,12 +6,17 @@ var manifest = {
     '../../javascripts/govuk/primary-links.js',
     '../../javascripts/govuk/stick-at-top-when-scrolling.js',
     '../../javascripts/govuk/stop-scrolling-at-footer.js',
-    '../../javascripts/govuk/selection-buttons.js'
+    '../../javascripts/govuk/selection-buttons.js',
+    '../../javascripts/govuk/analytics/google-analytics-universal-tracker.js',
+    '../../javascripts/govuk/analytics/analytics.js'
+
   ],
   test : [
     '../unit/MultivariateTestSpec.js',
     '../unit/PrimaryLinksSpec.js',
     '../unit/StickAtTopWhenScrollingSpec.js',
-    '../unit/SelectionButtonSpec.js'
+    '../unit/SelectionButtonSpec.js',
+    '../unit/analytics/GoogleAnalyticsUniversalTrackerSpec.js',
+    '../unit/analytics/AnalyticsSpec.js'
   ]
 };
