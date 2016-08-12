69e437d0c779b720cf0738d2ea9f5737c581d235
Henry Turner <henry.turner@digital.cabinet-office.gov.uk>
2016-08-12 11:32:01 +0000

    Allow both rspec matcher syntaxes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,4 +36,8 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
 end
