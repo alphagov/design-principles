87d935c59d5facfe2e8449e70c2047909618ce3f
Murray Steele <muz@h-lame.com>
2017-09-25 16:11:26 +0000

    Disable sessions and stop exposing secrets we don't use
    This app, like other frontend apps, does not use sessions so we don't need
    to have a session store, and we don't need to configure valid values for
    `secret_key_base` or `secret_token`.  However, rails will complain if we
    don't set anything for `secret_key_base`, so we can follow the lead of
    [frontend](https://github.com/alphagov/frontend/commit/98fb04c72473763087b02b2c5276eee13d8248df)
    and set a random value from `SecureRandom.hex`.  We can remove the old
    `secret_token` value too as we don't need it.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,5 +9,8 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Rails.application.config.secret_token = 'ee0d598834f5969b679cf6f0de4e31d8e5291c6f0f646f83c61bc4650f8d45723845c4c04baecb09d62c7847653bdeab7e8542a9bae306074def57c1e33e0a25'
-Rails.application.config.secret_key_base = 'ec75048af4551edebbe85f7071380cf4447eab834af85a26471faf8a29f723ba1a8cc36dbcfef2f08bb49c43a14f201d6cbc31c983035be955a58e687d5c7c96'
+
+# Because we don't use sessions we don't need a secret key base. Pick a random
+# one to satisfy Rails. If we ever were to start using sessions the key would
+# need to be made static.
+Rails.application.config.secret_key_base = SecureRandom.hex
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_design_principles_session'
+# Because we don't use sessions we can disable the session store entirely.
+Rails.application.config.session_store :disabled
